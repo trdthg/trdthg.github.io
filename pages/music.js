abc0b6a2acb8e2404a5ceb2031088a7b71afa9de
trdthg <trdthg@trdthg47@gmail.com>
2026-09-20 00:29:24 +0800

    Add music demo

--- pages/music.js
+++ pages/music.js
@@ -1,12 +1,32 @@
 // MUSIC 页面组件。
+
+MUSICS = [
+    {
+        name: "模仿星露谷物语 overture 的左手和弦，悲伤版本",
+        sheet: `3f  3a+ 4c+ 4f  4c+ 3b+ |
+3f  3b  4d+ 4f  4d+ 3b  |
+3d+ 3g+ 3b  4d+ 3b  3g+ |
+3d+ 3f+ 3a+ 4c+ 3a+ 3f+ |`
+    }
+]
+
 async function renderMusic() {
-    res = renderMD(`
+    const md = renderMD(`
         <h1>MUSIC</h1>
-        <p>会放一些我制作的音乐片段，还没想好怎么演示，不过大概会有下面的东西</p>
+        <p>会放一些我临时起意巧的的音乐片段，还没想好怎么演示，不过大概会有下面的东西</p>
 
-        - 一个自定义的音乐编程语言，也可能使用现有的语法
-        - 一个合成器，使用 fx7.js 或者 tone.js 即时播放
-
+        - [ ] 一些自定义的音乐 DSL，不过也可能使用现有的语法，比如 lilypond 之类的
+        - [ ] 一个合成器，使用 fx7.js 或者 tone.js 在线播放，这样就不用保存一堆 mp3 了
     `)
-    return res;
+
+    const cards = MUSICS.map(({ name, sheet }) => html`
+        <p>${name || "[FIXME!] empty name"}</p>
+        <pre>${sheet}</pre>
+    `);
+
+    return html`
+        <div>${md}</div>
+        <h3>片段们</h3>
+        ${cards}
+    `;
 }
--- pages/photo.js
+++ pages/photo.js
@@ -53,9 +53,9 @@ async function renderPhoto() {
 
     return html`
         <h1>PHOTO</h1>
-        <p>我不太爱拍照或者是看照片，不过它们还是挺珍贵的，所以勉强记录一下</p>
-        <p>所有的图片都不是原图，都是原图使用 imageMagick 缩小到 720p 并使用 75 的质量压缩成 webp 保存，最后每张照片基本都 ${"<100k"}, 我不喜欢大大的仓库</p>
-        <p>> 另外，这里只会发布我拍摄的照片，在 https://x.com/trdthg 有一些我绘制的画 或者 视频</p>
+        <p>既然拍了就还是挺珍贵的，也向你分享一下</p>
+        <p>所有的图片都不是原图，都使用 imageMagick 缩小到 720p 并使用 75 的质量压缩成 webp 保存，最后每张照片基本都 ${"<100k"}, 我不喜欢大大的仓库</p>
+        <p>> 另外，这里只会发布我拍摄的照片，在 <a href="https://x.com/trdthg">x.com/trdthg</a> 有一些我绘制的画或者上传的视频</p>
         <div id="gallery">
             ${cards.length ? cards : html`<p>还没想好做什么，先占个位。</p>`}
         </div>
